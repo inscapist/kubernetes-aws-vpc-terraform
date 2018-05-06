6117bdcafddd73e2c8b166e1b166b1bd5f258684
Felix Tioh Wei Lun <felixsagitta@gmail.com>
2018-05-06 18:12:01 +0800

    Update 02b_delete_cluster.sh

--- staging/kops/02b_delete_cluster.sh
+++ staging/kops/02b_delete_cluster.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-kops delete cluster --name k8i.intelllex.se --state s3://intelllex-se-kops-state --yes
+kops delete cluster --name k8i.intelllex.se --state s3://<KOPS_STATE_STORE> --yes
